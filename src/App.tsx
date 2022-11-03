3a0cac289e095b9b0ef6c2eb036610d5e6655458
Barry-san <oyeyemimubarak6@gmail.com>
2022-11-03 21:22:00 +0100

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import Landing from './components/Landing'
+import Navbar from './components/Navbar'
 import Register from './components/Register'
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './config/fconfig' 
+import { useEffect, useState } from 'react'
 
 function App() {
-
-
+  const [User, setUser] = useState('')
+  useEffect(()=>{
+    console.log(User)
+    onAuthStateChanged(auth, (user)=>{
+      if (user){
+        localStorage.setItem("user", JSON.stringify(user))
+        setUser("user")
+      }
+      else{
+        console.log('No user logged in');
+        setUser('')
+      }
+    })
+  }, [User])
+  
   return (
     <BrowserRouter>
+      <Navbar user="User" ></Navbar>
     <div className="App bg-slate-800 h-screen grid place-items-center">
       <Routes>
-        <Route path='/' element={<Landing/>}></Route>
         <Route path='/register' element={<Register/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
+        {User? 
+        <Route path='/' element={<Dashboard/>}></Route>
+         :
+        <Route path='/' element= {<Landing/>}></Route>
+      }
       </Routes>
     </div>
     </BrowserRouter>
