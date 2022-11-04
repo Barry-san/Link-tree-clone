bee3b6ff8739aeacef02c087deb2e63dbad30be7
Barry-san <oyeyemimubarak6@gmail.com>
2022-11-05 00:46:25 +0100

    loading component to be used for later

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+function Loading() {
+  return (
+    <div>
+      <p className="text-center text-gray-300 text-4xl animate-bounce">
+        Loading
+      </p>
+    </div>
+  );
+}
+
+export default Loading;
