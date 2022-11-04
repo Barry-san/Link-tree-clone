674410c051161b3303f4d952e3810bf415779ade
Barry-san <oyeyemimubarak6@gmail.com>
2022-11-05 00:56:16 +0100

    improved form validation and made login async

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,67 +1,77 @@
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { auth } from "../config/fconfig";
-import {signInWithEmailAndPassword} from "firebase/auth"
+import { signInWithEmailAndPassword } from "firebase/auth";
 type formValues = {
-    email:string,
-    password: string,
-}
+  email: string;
+  password: string;
+};
 function Login() {
-    const navigate = useNavigate()
-    const {register, handleSubmit} = useForm()
-    const onSubmit = (data:any)=>{
-        
-        signInWithEmailAndPassword(auth, data.email, data.password)
-        .then(()=>navigate('/'))
-        .catch(err=>{
-            console.log(err)
-            alert(err)
-        })
-        
-    }
-    return ( 
-        <>
-        <form 
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formValues>();
+  const onSubmit = async (data: formValues) => {
+    await signInWithEmailAndPassword(auth, data.email, data.password).catch(
+      (err) => {
+        console.log(err);
+        alert("sign in with email and password failed.");
+      }
+    );
+    alert("done");
+    navigate("/");
+  };
+  console.log(errors);
+  return (
+    <>
+      <form
         className="flex flex-col text-red-400 text-xl transit"
         onSubmit={handleSubmit(onSubmit)}
-        >
-            <div className="flex flex-col p-4 gap-2">
-                <label htmlFor="email">Email</label>
-                <input 
-                type="email"
-                required
-                {...register("email")}
-                id="email"
-                className="py-1 px-3 text-black"
-                 />
-            </div>
-            <div className="flex flex-col p-4 gap-2">
-                <label htmlFor="Password">Password</label>
-                <input 
-                type="password"
-                {...register('password')}
-                required
-                id="password"
-                className="py-1 px-3 text-black"
-                 />
-            </div>
-             <div className="flex gap-6 px-4 py-3 font-semibold">
-                <button
-                 type="submit" 
-                 className="py-2 bg-red-400 text-white flex-1 rounded-md"
-                 >
-                    Login
-                </button>
-                <button 
-                className="py-3 bg-white text-red-400 flex-1 rounded-md"
-                onClick={()=>navigate('/register')}
-                >
-                    Sign-up
-                </button>
-            </div>
-        </form>
-        </>
-     );
+      >
+        <div className="flex flex-col p-4 gap-2">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            id="email"
+            className="py-1 px-3 text-black"
+          />
+        </div>
+        <div className="flex flex-col p-4 gap-2">
+          <label htmlFor="Password">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 6,
+                message: "password must be at least 6 characters",
+              },
+            })}
+            required
+            id="password"
+            className="py-1 px-3 text-black"
+          />
+        </div>
+        <div className="flex gap-6 px-4 py-3 font-semibold">
+          <button
+            type="submit"
+            className="py-2 bg-red-400 text-white flex-1 rounded-md"
+          >
+            Login
+          </button>
+          <button
+            className="py-3 bg-white text-red-400 flex-1 rounded-md"
+            onClick={() => navigate("/register")}
+          >
+            Sign-up
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Login;
