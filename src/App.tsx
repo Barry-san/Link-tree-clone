aca7f571fc289af307b09f3d3af7062521b5f2dd
Barry-san <oyeyemimubarak6@gmail.com>
2022-11-05 00:54:58 +0100

    added auth listener and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,55 @@
-import Landing from './components/Landing'
-import Navbar from './components/Navbar'
-import Register from './components/Register'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
-import About from './components/About'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './config/fconfig' 
-import { useEffect, useState } from 'react'
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
+import Boundary from "./components/Boundary";
+import Profiles from "./components/Profiles";
+import Addlinks from "./components/Addlinks";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/fconfig";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
 
 function App() {
-  const [User, setUser] = useState('')
-  useEffect(()=>{
-    console.log(User)
-    onAuthStateChanged(auth, (user)=>{
-      if (user){
-        localStorage.setItem("user", JSON.stringify(user))
-        setUser("user")
+  const [User, setUser] = useState("");
+  useEffect(() => {
+    console.log(User);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        localStorage.setItem("userEmail", JSON.stringify(user.email));
+        setUser("user");
+      } else {
+        console.log("No user logged in");
+        setUser("");
       }
-      else{
-        console.log('No user logged in');
-        setUser('')
-      }
-    })
-  }, [User])
-  
+    });
+  }, [User]);
+
   return (
     <BrowserRouter>
-      <Navbar user="User" ></Navbar>
-    <div className="App bg-slate-800 h-screen grid place-items-center">
-      <Routes>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-
-        {User? 
-        <Route path='/' element={<Dashboard/>}></Route>
-         :
-        <Route path='/' element= {<Landing/>}></Route>
-      }
-      </Routes>
-    </div>
+      <Navbar user={User}></Navbar>
+      <div className="App bg-slate-800 min-h-screen min-w-full grid place-items-center">
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/@:username" element={<Profiles />}></Route>
+          {User ? (
+            <Route path="/" element={<Dashboard />}>
+              <Route path="mylinks"></Route>
+              <Route path="addlinks" element={<Addlinks />}></Route>
+            </Route>
+          ) : (
+            <Route path="/" element={<Landing />}></Route>
+          )}
+          <Route path="*" element={<Boundary />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
