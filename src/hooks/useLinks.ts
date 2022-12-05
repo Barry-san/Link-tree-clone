04f3f24e056b89dd7886a71e470efa4d20abef21
Barry-san <oyeyemimubarak6@gmail.com>
2022-12-05 13:52:39 +0100

    worked on useLink hook'

--- src/hooks/useLinks.ts
+++ src/hooks/useLinks.ts
@@ -1,13 +1,22 @@
 import { getDocs, collection } from "firebase/firestore";
+import { useState } from "react";
 import { db } from "../config/fconfig";
 
-export async function useLinks(username: string) {
+export function useLinks(username: string) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   const docref = collection(db, "usernames", username, "links");
-  const linky = await getDocs(docref);
-  const result: any = [];
-  linky.forEach((link) => {
-    result.push(link.data());
-  });
-  console.log(result);
-  return result;
+  try {
+    getDocs(docref).then((data) => {
+      setData(data);
+      setLoading(false);
+    });
+  } catch (err: any) {
+    setError(err);
+    setLoading(false);
+    console.log("this is the error ", error);
+  }
+
+  return { data, loading, error };
 }
